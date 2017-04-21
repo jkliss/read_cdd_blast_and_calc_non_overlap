3ea506b4b6f97ff5b0d8e6fe231ccde0af5e0a18
jk <julius.kliss@gmail.com>
2017-04-21 09:51:45 +0200

    <

--- src/CDDetectReader.java
+++ src/CDDetectReader.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Created by students on 07.04.17.
@@ -14,8 +12,9 @@ import java.util.regex.Pattern;
  */
 public class CDDetectReader {
     Map<String, List<int[]>> CDMap = new HashMap<String, List<int[]>>(10000000);
+    Map<String, Boolean> fullGeneCD = new HashMap<String, Boolean>(1000000);
 
-    public CDDetectReader(String filename){
+    public void initCDDetectReader(String filename) {
         BufferedReader br = null;
         FileReader fr = null;
         try {
@@ -23,23 +22,20 @@ public class CDDetectReader {
             br = new BufferedReader(fr);
             String sCurrentLine;
             br = new BufferedReader(new FileReader(filename));
-            Pattern pattern = Pattern.compile("([^\\s]+)\\((\\d+),(\\d+)\\)");
             while ((sCurrentLine = br.readLine()) != null) {
-                Matcher match = pattern.matcher(sCurrentLine);
-                match.find();
                 List<int[]> currentList = new ArrayList<int[]>();
-                try{
-                    if(CDMap.get(match.group(1)) != null){
-                        currentList = CDMap.get(match.group(1));
+                String[] splits = sCurrentLine.split("\t");
+                try {
+                    if (CDMap.get(splits[0]) != null) {
+                        currentList = CDMap.get(splits[0]);
                     }
-                    if(!currentList.contains(new int[]{Integer.parseInt(match.group(2)), Integer.parseInt(match.group(3))})){
-                        currentList.add(new int[]{Integer.parseInt(match.group(2)), Integer.parseInt(match.group(3))});
-                        CDMap.put(match.group(1), currentList);
-                        //System.out.println(match.group(1) + " " + match.group(2) + " " + match.group(3));
+                    if (fullGeneCD.containsKey(splits[7]) && !currentList.contains(new int[]{Integer.parseInt(splits[3]), Integer.parseInt(splits[4])})) {
+                        currentList.add(new int[]{Integer.parseInt(splits[4]), Integer.parseInt(splits[4])});
+                        CDMap.put(splits[0], currentList);
                     }
-                } catch (IllegalStateException ex){
+                } catch (IllegalStateException ex) {
                     ex.printStackTrace();
-                    System.err.println("Wrong Format ... ( has to be CDD(X,X) )");
+                    System.err.println("Wrong Format ... COL: 0 Protname; COL: 3 Alignment Start; COL: 4 Alignment End; COL: 7 CDname");
                     System.err.println(sCurrentLine);
                 }
             }
@@ -57,45 +53,72 @@ public class CDDetectReader {
         }
     }
 
-    public void calc_overlap(){
+    public void calc_overlap() {
         for (String key : CDMap.keySet()) {
             boolean fnd = false;
             List<int[]> list = CDMap.get(key);
             boolean[] hasNoOverlap = new boolean[list.size()];
-            for(int i = 0; i < list.size()-1; i++){
+            for (int i = 0; i < list.size() - 1; i++) {
                 int[] ints1 = list.get(i);
-                for(int j = i+1; j < list.size(); j++){
+                for (int j = i + 1; j < list.size(); j++) {
                     int[] ints2 = list.get(j);
-                    if((ints1[0] > ints2[1] && ints1[1] > ints2[1] ) || (ints2[0] > ints1[1] && ints2[1] > ints1[1])){
+                    if ((ints1[0] > ints2[1] && ints1[1] > ints2[1]) || (ints2[0] > ints1[1] && ints2[1] > ints1[1])) {
                         hasNoOverlap[i] = true;
                         hasNoOverlap[j] = true;
                         fnd = true;
                     }
                 }
             }
-            if(fnd){
+            if (fnd) {
                 print_subset(key, list, hasNoOverlap);
             }
         }
     }
 
-    public void print_set(String key){
+    public void print_set(String key) {
         String print = key + "\t";
         for (int[] ints : CDMap.get(key)) {
-            if(!print.contains("(" + ints[0] + "," + ints[1] + ")")){
+            if (!print.contains("(" + ints[0] + "," + ints[1] + ")")) {
                 print = print + "(" + ints[0] + "," + ints[1] + ")";
             }
         }
         System.out.println(print);
     }
 
-    public void print_subset(String key, List<int[]> list,boolean[] hasNoOverlap){
+    public void print_subset(String key, List<int[]> list, boolean[] hasNoOverlap) {
         String print = key + "\t";
         for (int i = 0; i < list.size(); i++) {
-            if(hasNoOverlap[i] && !print.contains("(" + list.get(i)[0] + "," + list.get(i)[1] + ")")){
+            if (hasNoOverlap[i] && !print.contains("(" + list.get(i)[0] + "," + list.get(i)[1] + ")")) {
                 print = print + "(" + list.get(i)[0] + "," + list.get(i)[1] + ")";
             }
         }
         System.out.println(print);
+    }
+
+    public void setFullGeneCD(String filename) {
+        BufferedReader br = null;
+        FileReader fr = null;
+        try {
+            fr = new FileReader(filename);
+            br = new BufferedReader(fr);
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(filename));
+            while ((sCurrentLine = br.readLine()) != null) {
+                sCurrentLine = sCurrentLine.replace("\n", "");
+                fullGeneCD.put(sCurrentLine, true);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+                if (fr != null)
+                    fr.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
 }
--- src/read_cdd_blast_and_calc_non_overlap.java
+++ src/read_cdd_blast_and_calc_non_overlap.java
@@ -7,11 +7,13 @@ public class read_cdd_blast_and_calc_non_overlap {
             BlastReader breader = new BlastReader(args[0]);
             breader.calc_overlap();
         } else if(args.length > 1){
-            CDDetectReader cdreader = new CDDetectReader(args[0]);
-            cdreader.calc_overlap();
+            CDDetectReader cdReader = new CDDetectReader();
+            cdReader.setFullGeneCD(args[1]);
+            cdReader.initCDDetectReader(args[0]);
+            cdReader.calc_overlap();
         }
         else {
-            System.err.println("No File!\nuse -n for only CD File");
+            System.err.println("No File!\nUse one Argument for BLAST output as Input\nUse Two Arguments for only CD File + Full Gene File");
         }
 
     }
