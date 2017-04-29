78bee72cc359211168c7900ce37948c26e7c93db
jkliss <julius.kliss@gmail.com>
2017-04-29 14:16:20 +0200

    <

--- src/read_cdd_blast_and_calc_non_overlap.java
+++ src/read_cdd_blast_and_calc_non_overlap.java
@@ -35,7 +35,7 @@ public class read_cdd_blast_and_calc_non_overlap {
 
         }
         else {
-            System.err.println("No File!\nUse one Argument for BLAST output as Input\nUse Two Arguments for only CD File + Full Gene File");
+            System.err.println("No File!\nUse one Argument for BLAST output as Input\nUse Two Arguments for only CD File + Full Gene File\nUse Three Arguments for CD File + Fullgene Names + Fullgene Sequences(CD Length Comparison)");
         }
     }
 }
