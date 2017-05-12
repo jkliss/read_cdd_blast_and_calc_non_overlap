b79597124c331c94a79c38f40bc471dedbec9d2e
jk <julius.kliss@gmail.com>
2017-05-12 14:24:11 +0200

    <

--- src/NCBI_Detection.java
+++ src/NCBI_Detection.java
@@ -13,14 +13,22 @@ public class NCBI_Detection {
     public static void main(String[] args) {
         if(args.length > 0){
             NCBI_Detection ncbi_detection = new NCBI_Detection(new SeqReader("GCF_000974405.1_ASM97440v1_protein.faa.small_heads"));
+            if(args.length > 1){
+                ncbi_detection = new NCBI_Detection(new SeqReader("GCF_000974405.1_ASM97440v1_protein.faa.small_heads"), Integer.parseInt(args[1]));
+            }
             ncbi_detection.start();
         } else {
-            System.err.println("Argument 1: Input Sequence File");
+            System.err.println("Argument 1: Input Sequence File\nOptional Argument Number of threads (std=8)");
         }
     }
 
     public NCBI_Detection(SeqReader sequences){
         this.sequences = sequences;
+    }
+
+    public NCBI_Detection(SeqReader sequences, int numThreads){
+        this.sequences = sequences;
+        threads = new Thread[numThreads];
     }
 
     public void start(){
@@ -48,9 +56,9 @@ public class NCBI_Detection {
 
     public void startAll(){
         for(int z = 0; z <= set;){
-            for(int i = 0; i < 8; i++){
+            for(int i = 0; i < threads.length; i++){
                 if(threads[i] == null || !threads[i].isAlive()){
-                    System.err.println("Thread " + z);
+                    //System.err.println("Thread " + z);
                     threads[i] = scheduler(z);
                     threads[i].start();
                     System.err.println("Thread started...");
@@ -64,7 +72,7 @@ public class NCBI_Detection {
                 }
             }
         }
-        for(int i = 0; i < 8; i++){
+        for(int i = 0; i < threads.length; i++){
             if(threads[i] != null && threads[i].isAlive()){
                 i = 0;
                 try {
@@ -82,7 +90,7 @@ public class NCBI_Detection {
                 Runtime r = Runtime.getRuntime();
                 try{
                     System.err.println("Protein Set " + i + " started.");
-                    Process p = r.exec("perl mod_bwrpsd.pl < proteinSet_" + i + " > proteinSet_" + i + ".out");
+                    Process p = r.exec("perl mod_bwrpsd.pl < proteinSet_" + i + " 1> proteinSet_" + i + ".out 2> proteinSet_" + i + ".err" );
                     p.waitFor();
                     System.err.println(p.getErrorStream().toString());
                 } catch (IOException ex){
@@ -90,9 +98,10 @@ public class NCBI_Detection {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.err.println("Protein Set " + i + " ended");
             }
         });
-        System.err.println("Protein Set " + i + " ended");
+
         return thread;
     }
 }
